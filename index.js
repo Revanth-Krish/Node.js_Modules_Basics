d2a4964e38b58e54a80bd160e19911a1aad6d56e
Revanth <147156884+Revanth-Krish@users.noreply.github.com>
2023-10-11 07:09:34 +0000

    Pending changes exported from your codespace

---
+++ index.js
@@ -0,0 +1,10 @@
+//let x = 12345
+//let y = 45345
+//let result = x + y
+//console.log(`sum of ${x} and ${y} is ${result} `)
+
+
+let num1 = parseInt(process.argv[2]);
+let num2 = parseInt(process.argv[3]);
+let sum = num1 + num2;
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
